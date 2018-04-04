cd05f4c5f62699c3618d064a1aad685bef40d42a
Pim Wingelaar <p.wingelaar@sourcexs.nl>
2018-04-04 10:01:34 +0200

    Added check for adding same book

--- BoekLibaryTests/BoekenwinkelTests.cs
+++ BoekLibaryTests/BoekenwinkelTests.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using BoekLibary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace BoekLibary.Tests
+namespace BoekLibaryTests
 {
     [TestClass()]
     public class BoekenwinkelTests
@@ -8,11 +10,29 @@ namespace BoekLibary.Tests
         [TestMethod()]
         public void NieuwBoekTest()
         {
-            Afmeting testAfmeting = new Afmeting(5, 5, 5);
+            var testAfmeting = new Afmeting(5, 5, 5);
             var testBoek = new Boek("harry_potter_and_the_philospher_stone", "j.k.rowling", EnumTaal.Engels, 290, 12.49, testAfmeting, 1408855928, 20, 50, 34, "1", 1);
             Boekenwinkel.NieuwBoek(testBoek);
 
             Equals(Product.Boekenlijst.Count == 1);
+        }
+
+        [TestMethod()]
+        public void NieuwTijdschriftTest()
+        {
+            var testAfmeting = new Afmeting(5, 5, 5);
+            var testtijdschrift = new Tijdschrift("donald_duck", "walt disney", EnumTaal.Nederlands, 140, 4.99, testAfmeting, "16-02-2018", "20-02-2018", 54987618, 200, 1);
+            Boekenwinkel.NieuwTijdschrift(testtijdschrift);
+
+            Equals(Product.Tijdschriftenlijst.Count == 1);
+        }
+
+        [TestMethod()]
+        public void BoekenwinkelTest()
+        {
+            var boekenwinkel = new Boekenwinkel(1, "Tel - 0612923821 | Email - test@boekenwinkel.nl  | Adres - Testlaan 1 Arnhem", "8:00 - 16:00");
+
+            Assert.IsNotNull(boekenwinkel);
         }
     }
 }   
